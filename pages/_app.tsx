3a822bc000cd72827256e9f2c702adaf3709c85b
Ryan Rudd <131509539+Ryan-Rudd@users.noreply.github.com>
2025-12-02 23:09:50 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,35 +4,3 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-import { useEffect } from "react";
-
-function Header() {
-  useEffect(() => {
-    const header = document.querySelector("header");
-    if (header) {
-      header.style.fontFamily = "'Roboto', sans-serif";
-      header.style.fontSize = "2rem";
-      header.style.fontWeight = "bold";
-      header.style.color = "#333";
-      header.style.textAlign = "center";
-      header.style.padding = "20px";
-      header.style.backgroundColor = "#f0f0f0";
-    }
-  }, []);
-
-  return (
-    <header>
-      <h1>Welcome to Your Next Adventure!</h1>
-    </header>
-  );
-}
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
-}
