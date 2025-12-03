134a2eb271bf6628745e7c6d4bfcef31441643c0
PushPilot <pushpilot@example.com>
2025-12-03 01:17:21 +0000

    Update Greeting Text
    Change the text in the index.tsx file from 'To get started, edit the index.tsx file' to 'Hello World!'.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
           }
         `}</style>
       </Head>
-      <nav>SRDreams</nav>
+      <nav>Hello World!</nav>
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-            To get started, edit the index.tsx file.
+            Hello World!
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600">
             Looking for a starting point or more instructions? Head over to{" "}
