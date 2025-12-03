4628e9fa7eb2772c2f0ebe76097e08eb78291648
PushPilot <pushpilot@example.com>
2025-12-03 01:15:32 +0000

    Update Home Page Navigation Bar
    Change the navigation bar on the home page to have a white background and update the text to 'SRDreams'.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,32 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style>{`
+            body { margin: 0 }
+            .navbar { background-color: white; }
+            .navbar a { color: black; }
+          `}</style>
+        </Head>
+        <body className="antialiased">
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">SRDreams</a>
+          </nav>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
